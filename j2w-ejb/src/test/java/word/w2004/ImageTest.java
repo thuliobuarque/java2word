dc3c721ff9ed106ce9ec90b496a1d4ac7df02ba1
leonardoanalista <leonardoanalista@7405c908-b26d-b50d-16d8-df60715af405>
2010-08-03 12:56:00 +0000

    img test code for base64.....

--- j2w-ejb/src/test/java/word/w2004/ImageTest.java
+++ j2w-ejb/src/test/java/word/w2004/ImageTest.java
@@ -1,12 +1,23 @@
 package word.w2004;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
 import junit.framework.Assert;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import word.utils.TestUtils;
 import word.utils.Utils;
 import word.w2004.elements.Image;
+import org.jboss.seam.util.Base64;
 
 public class ImageTest extends Assert {
 
@@ -20,10 +31,10 @@ public class ImageTest extends Assert {
 		assertEquals(2, TestUtils.regexCount(img.getContent(), "<*w:pict>"));
 		assertEquals(2, TestUtils.regexCount(img.getContent(), "<*v:shapetype"));
 		assertEquals(2, TestUtils.regexCount(img.getContent(), "<*v:shape[ >]")); //white space or >
-		assertEquals(2, TestUtils.regexCount(img.getContent(), "wordml"));		
+		assertEquals(2, TestUtils.regexCount(img.getContent(), "wordml"));
 		//for dtPicker.gif
 		assertEquals(1, TestUtils.regexCount(img.getContent(), "R0lGODlhEAAQAKIAAKVNSkpNpUpNSqWmpdbT1v///////wAAACH5BAEAAAYALAAAAAAQABAAAANE\naLrcNjDKKUa4OExYM95DVRTEWJLmKKLseVZELMdADcSrOwK7OqQsXkEIm8lsN0IOqCssW8Cicar8\nQa/P5kvA7Xq/ggQAOw=="));
-		
+
 	}
 
 	@Test
@@ -76,6 +87,30 @@ public class ImageTest extends Assert {
 		System.out.println("IIOException is expected ok...");
 		String res = img.getOriginalWidthHeight();
 		assertEquals("error", res);
+	}
+
+	@Ignore
+	@Test
+	public void xxxxx() throws IOException{
+
+
+		URL url = new URL("http://localhost:8080/j2w/img/dtpick2.gif");
+		BufferedImage img = ImageIO.read(url);// throws IIOException
+		System.out.println("H: " + img.getHeight());
+		System.out.println("W: " + img.getWidth());
+
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream(1000);
+		//RenderedImage aBufferedImage = null;
+		ImageIO.write(img, "gif" /* "png" "jpeg" ... format desired */,
+		           baos );
+		baos.flush();
+		byte[] resultImageAsRawBytes = baos.toByteArray();
+
+
+		String encodedString = Base64.encodeBytes(resultImageAsRawBytes);
+
+		System.out.println(encodedString);
 	}
 
 }
