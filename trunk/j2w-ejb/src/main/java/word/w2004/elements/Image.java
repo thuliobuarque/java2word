741c79600d8f19c51236fd3eb044f5d9db5ecfdc
leonardoanalista <leonardoanalista@7405c908-b26d-b50d-16d8-df60715af405>
2010-08-07 01:23:26 +0000

    Added image URL, deprecatet image constructor without ImageType, Unit tests updated,

--- trunk/j2w-ejb/src/main/java/word/w2004/elements/Image.java
+++ trunk/j2w-ejb/src/main/java/word/w2004/elements/Image.java
@@ -3,6 +3,7 @@ package word.w2004.elements;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import javax.imageio.ImageIO;
 
@@ -16,66 +17,89 @@ public class Image implements IImage {
 	static Logger log = Logger.getLogger(Image.class);
 	private StringBuilder txt = new StringBuilder("");
 	private boolean hasBeenCalledBefore = false;
-	private String fullPath = "";
-	private String width = "";
-	private String height = "";
-
+	private String path = "";
+	private String width = ""; // to be able to set this to override default
+								// size
+	private String height = ""; // to be able to set this to override default
+								// size
+	BufferedImage bufferedImage;
+	
 	/**
-	 * @param fullPath
-	 * Full path absolute (from the root of your server.) including file name and extension.
-	 * In the future, I will implement relative path and external url.
+	 * Use the other constructor and specify the image type: FULL_LOCAL_PATH or WEB_URL.
 	 * 
-	 * If you deploy unpacked, you can presume the full path
-	 * If you deploy packed EAR/WAR, you probably will have to put the image out of your application in somewhere in your server.
+	 * FULL_LOCAL_PATH: It has to start from the root of your system 
+	 * WEB_URL: it can be http://localhost/your_app/img/xxx.gif or http://google.com/img/logoWhatever.png
 	 * 
-	 * Sorry for the inconvenient, I will fix this in the next releases... 
-	 *   
+	 * This constructor will be removed in the next version. 
 	 */
-	public Image(String fullPath) {
-		this.fullPath = fullPath;
+	@Deprecated 
+	public Image(String path) {
+		this(path, ImageType.FULL_LOCAL_PATH);
 	}
 
-	public String getOriginalWidthHeight(){
-		//URL url = new URL("http://localhost:8080/app02/img/dtpick.gif");
-		BufferedImage img;
+	/**
+	 * 
+	 * @param path
+	 * 
+	 * This is the location on your image. If you specify  "imageType" as WEB_URL, your path should start with "http://..."
+	 * But if you choose "imageType" as FULL_LOCAL_PATH, you should specify path value as absolute path from the root of your server. Eg.: /Users/YourName/imgs/... 
+	 * 
+	 * @param imageType
+	 * FULL_LOCAL_PATH:  Full path absolute (from the root of your server.) including file name and extension. 
+	 * It has to start from the root of your system. 
+	 * 
+	 * WEB_URL: It can be http://localhost/your_app/img/xxx.gif or http://google.com/img/logoWhatever.png
+	 */
+	public Image(String path, ImageType imageType) {
+		this.path = path;
 		try {
-			img = ImageIO.read(new File(fullPath));
+			if (imageType.equals(ImageType.FULL_LOCAL_PATH)) {
+				bufferedImage = ImageIO.read(new File(path));
+			}
+			if (imageType.equals(ImageType.WEB_URL)) {
+				URL url = new URL(path);
+				bufferedImage = ImageIO.read(url);
+			}
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-			//throw new IOException(e); 
-			return "error";
+			throw new RuntimeException("Can't create ImageIO.", e);
 		}
-		String res = img.getWidth() + "#" + img.getHeight() + "";
+	}
+
+	public String getOriginalWidthHeight() {
+		String res = bufferedImage.getWidth() + "#" + bufferedImage.getHeight()
+				+ "";
 		log.debug(res);
 		return res;
 	}
-	
-	private void setUpSize(){
-		if("".equals(this.width) || "".equals(this.height)){
-			String [] wh = getOriginalWidthHeight().split("#");
+
+	private void setUpSize() {
+		if ("".equals(this.width) || "".equals(this.height)) {
+			String[] wh = getOriginalWidthHeight().split("#");
 			String ww = wh[0];
 			String hh = wh[1];
-			if("".equals(this.width)){
-				this.width = ww; 
+			if ("".equals(this.width)) {
+				this.width = ww;
 			}
-			if("".equals(this.height)){
-				this.height = hh; 
+			if ("".equals(this.height)) {
+				this.height = hh;
 			}
 		}
-		
+
 	}
-	
+
 	public String getContent() {
-		if(hasBeenCalledBefore ){
-			return txt.toString();	
-		}else{
+		if (hasBeenCalledBefore) {
+			return txt.toString();
+		} else {
 			hasBeenCalledBefore = true;
 		}
 		// Placeholders: internalFileName, fileName, binary, width and height
 
-		log.debug("@@@ fullPath: " + fullPath);
+		log.debug("@@@ path: " + path);
 
-		String[] arr = fullPath.split("/");
+		String[] arr = path.split("/");
 		String fileName = arr[arr.length - 1];
 
 		log.debug("@@@ fileName: " + fileName);
@@ -83,30 +107,34 @@ public class Image implements IImage {
 		String internalFileName = System.currentTimeMillis() + fileName;
 		log.debug(internalFileName);
 
-		String binary = ImageUtils.getImageHexaBase64(fullPath);
+		// String binary = ImageUtils.getImageHexaBase64(path);
+		String imageformat = path.substring(path.lastIndexOf('.') + 1);
+		String binary = ImageUtils.getImageHexaBase64(bufferedImage,
+				imageformat);
 		log.debug("@@@ Bynary: " + binary);
 
 		setUpSize();
 
-		String res = img;
+		String res = img_template;
 		res = res.replace("{fileName}", fileName);
 		res = res.replace("{internalFileName}", internalFileName);
 		res = res.replace("{binary}", binary);
 		res = res.replace("{width}", this.width);
 		res = res.replace("{height}", this.height);
-		
+
 		txt.append(res);
 		return txt.toString();
 	}
-	
-	public void setWidth(String value){
+
+	public void setWidth(String value) {
 		this.width = value;
 	}
-	public void setHeight(String value){
+
+	public void setHeight(String value) {
 		this.height = value;
 	}
 
-	private String img = "\n<w:pict>"
+	private String img_template = "\n<w:pict>"
 			+ "\n	<v:shapetype id=\"_x0000_t75\" coordsize=\"21600,21600\" o:spt=\"75\" o:preferrelative=\"t\" path=\"m@4@5l@4@11@9@11@9@5xe\" filled=\"f\" stroked=\"f\">"
 			+ "		<v:stroke joinstyle=\"miter\"/>"
 			+ "		<v:formulas>"
@@ -129,47 +157,51 @@ public class Image implements IImage {
 			+ "\n	<v:shape id=\"_x0000_i1026\" type=\"#_x0000_t75\" style=\"width:{width}pt;height:{height}pt\"><v:imagedata src=\"wordml://{internalFileName}\" o:title=\"{fileName}\"/>"
 			+ "\n	</v:shape>" + "\n</w:pict>";
 
-/*	
-	private String imgTst = "\n<w:pict>"
-			+ "\n	<v:shapetype id=\"_x0000_t75\" coordsize=\"21600,21600\" o:spt=\"75\" o:preferrelative=\"t\" path=\"m@4@5l@4@11@9@11@9@5xe\" filled=\"f\" stroked=\"f\"><v:stroke joinstyle=\"miter\"/><v:formulas><v:f eqn=\"if lineDrawn pixelLineWidth 0\"/><v:f eqn=\"sum @0 1 0\"/><v:f eqn=\"sum 0 0 @1\"/><v:f eqn=\"prod @2 1 2\"/><v:f eqn=\"prod @3 21600 pixelWidth\"/><v:f eqn=\"prod @3 21600 pixelHeight\"/><v:f eqn=\"sum @0 0 1\"/><v:f eqn=\"prod @6 1 2\"/><v:f eqn=\"prod @7 21600 pixelWidth\"/><v:f eqn=\"sum @8 21600 0\"/><v:f eqn=\"prod @7 21600 pixelHeight\"/><v:f eqn=\"sum @10 21600 0\"/></v:formulas><v:path o:extrusionok=\"f\" gradientshapeok=\"t\" o:connecttype=\"rect\"/><o:lock v:ext=\"edit\" aspectratio=\"t\"/></v:shapetype>"
-			+ "\n<w:binData w:name=\"wordml://01000002.gif\" xml:space=\"preserve\">R0lGODlhHwAaAMQAAMjIyOHh4cLCwufn56ioqKOjo+zs7L29va6urs3NzdLS0ri4uNfX19zc3LOz"
-			+ "s/Hx8Z6engAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAAA"
-			+ "AAAALAAAAAAfABoAAAWv4COOZDCQ6Gg0aRs40NGSQAGd82MIUH/PDYJPNgsgfL1FC4DsBVy2JoRF"
-			+ "OkghjtQgKhWQeNcCbgSTIqiixhUCMKDU3VQZSXimrM2EVkpwt7g9WSkJUmgoAVIKLQtNBDkMUn4o"
-			+ "R0heM0xNDpqalnybmnYPmGtYIqQ+aKNrgqc9qa2sra+nsaezpLWkt6umsiOqV7lru8G9tr+wxrrI"
-			+ "tMrDIwqf0pwi09OhOdna29wpIQA7"
-			+ "</w:binData>"
-			+ "\n	<v:shape id=\"_x0000_i1026\" type=\"#_x0000_t75\" style=\"width:31pt;height:26pt\"><v:imagedata src=\"wordml://01000002.gif\" o:title=\"quote.gif\"/>"
-			+ "\n	</v:shape>" + "\n</w:pict>";
-
-	private String img02 = "\n<w:pict> "
-			+ "\n	<v:shapetype id=\"_x0000_t75\" coordsize=\"21600,21600\" o:spt=\"75\" o:preferrelative=\"t\" path=\"m@4@5l@4@11@9@11@9@5xe\" filled=\"f\" stroked=\"f\"> "
-			+ "\n		<v:stroke joinstyle=\"miter\"/><v:formulas><v:f eqn=\"if lineDrawn pixelLineWidth 0\"/> "
-			+ "\n		<v:f eqn=\"sum @0 1 0\"/> "
-			+ "\n		<v:f eqn=\"sum 0 0 @1\"/> "
-			+ "\n		<v:f eqn=\"prod @2 1 2\"/> "
-			+ "\n		<v:f eqn=\"prod @3 21600 pixelWidth\"/> "
-			+ "\n		<v:f eqn=\"prod @3 21600 pixelHeight\"/> "
-			+ "\n		<v:f eqn=\"sum @0 0 1\"/> "
-			+ "\n		<v:f eqn=\"prod @6 1 2\"/> "
-			+ "\n		<v:f eqn=\"prod @7 21600 pixelWidth\"/> "
-			+ "\n		<v:f eqn=\"sum @8 21600 0\"/> "
-			+ "\n		<v:f eqn=\"prod @7 21600 pixelHeight\"/> "
-			+ "\n		<v:f eqn=\"sum @10 21600 0\"/> "
-			+ "\n	</v:formulas> "
-			+ "\n		<v:path o:extrusionok=\"f\" gradientshapeok=\"t\" o:connecttype=\"rect\"/> "
-			+ "\n		<o:lock v:ext=\"edit\" aspectratio=\"t\"/> "
-			+ "\n	</v:shapetype> "
-			+ "\n<w:binData w:name=\"wordml:\01000002.gif\" xml:space=\"preserve\">"
-			+ "R0lGODlhHwAaAMQAAMjIyOHh4cLCwufn56ioqKOjo+zs7L29va6urs3NzdLS0ri4uNfX19zc3LOz"
-			+ "s/Hx8Z6engAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAAA"
-			+ "AAAALAAAAAAfABoAAAWv4COOZDCQ6Gg0aRs40NGSQAGd82MIUH/PDYJPNgsgfL1FC4DsBVy2JoRF"
-			+ "OkghjtQgKhWQeNcCbgSTIqiixhUCMKDU3VQZSXimrM2EVkpwt7g9WSkJUmgoAVIKLQtNBDkMUn4o"
-			+ "R0heM0xNDpqalnybmnYPmGtYIqQ+aKNrgqc9qa2sra+nsaezpLWkt6umsiOqV7lru8G9tr+wxrrI"
-			+ "tMrDIwqf0pwi09OhOdna29wpIQA7"
-			+ "</w:binData>"
-			+ "\n	<v:shape id=\"_x0000_i1026\" type=\"#_x0000_t75\" style=\"width:31pt;height:26pt\"><v:imagedata src=\"wordml:\01000002.gif\" o:title=\"quote.gif\"/>"
-			+ "\n	</v:shape>" + "\n</w:pict>";
-
-*/
+	/*
+	 * private String imgTst = "\n<w:pict>" +
+	 * "\n	<v:shapetype id=\"_x0000_t75\" coordsize=\"21600,21600\" o:spt=\"75\" o:preferrelative=\"t\" path=\"m@4@5l@4@11@9@11@9@5xe\" filled=\"f\" stroked=\"f\"><v:stroke joinstyle=\"miter\"/><v:formulas><v:f eqn=\"if lineDrawn pixelLineWidth 0\"/><v:f eqn=\"sum @0 1 0\"/><v:f eqn=\"sum 0 0 @1\"/><v:f eqn=\"prod @2 1 2\"/><v:f eqn=\"prod @3 21600 pixelWidth\"/><v:f eqn=\"prod @3 21600 pixelHeight\"/><v:f eqn=\"sum @0 0 1\"/><v:f eqn=\"prod @6 1 2\"/><v:f eqn=\"prod @7 21600 pixelWidth\"/><v:f eqn=\"sum @8 21600 0\"/><v:f eqn=\"prod @7 21600 pixelHeight\"/><v:f eqn=\"sum @10 21600 0\"/></v:formulas><v:path o:extrusionok=\"f\" gradientshapeok=\"t\" o:connecttype=\"rect\"/><o:lock v:ext=\"edit\" aspectratio=\"t\"/></v:shapetype>"
+	 * +
+	 * "\n<w:binData w:name=\"wordml://01000002.gif\" xml:space=\"preserve\">R0lGODlhHwAaAMQAAMjIyOHh4cLCwufn56ioqKOjo+zs7L29va6urs3NzdLS0ri4uNfX19zc3LOz"
+	 * +
+	 * "s/Hx8Z6engAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAAA"
+	 * +
+	 * "AAAALAAAAAAfABoAAAWv4COOZDCQ6Gg0aRs40NGSQAGd82MIUH/PDYJPNgsgfL1FC4DsBVy2JoRF"
+	 * +
+	 * "OkghjtQgKhWQeNcCbgSTIqiixhUCMKDU3VQZSXimrM2EVkpwt7g9WSkJUmgoAVIKLQtNBDkMUn4o"
+	 * +
+	 * "R0heM0xNDpqalnybmnYPmGtYIqQ+aKNrgqc9qa2sra+nsaezpLWkt6umsiOqV7lru8G9tr+wxrrI"
+	 * + "tMrDIwqf0pwi09OhOdna29wpIQA7" + "</w:binData>" +
+	 * "\n	<v:shape id=\"_x0000_i1026\" type=\"#_x0000_t75\" style=\"width:31pt;height:26pt\"><v:imagedata src=\"wordml://01000002.gif\" o:title=\"quote.gif\"/>"
+	 * + "\n	</v:shape>" + "\n</w:pict>";
+	 * 
+	 * private String img02 = "\n<w:pict> " +
+	 * "\n	<v:shapetype id=\"_x0000_t75\" coordsize=\"21600,21600\" o:spt=\"75\" o:preferrelative=\"t\" path=\"m@4@5l@4@11@9@11@9@5xe\" filled=\"f\" stroked=\"f\"> "
+	 * +
+	 * "\n		<v:stroke joinstyle=\"miter\"/><v:formulas><v:f eqn=\"if lineDrawn pixelLineWidth 0\"/> "
+	 * + "\n		<v:f eqn=\"sum @0 1 0\"/> " + "\n		<v:f eqn=\"sum 0 0 @1\"/> " +
+	 * "\n		<v:f eqn=\"prod @2 1 2\"/> " +
+	 * "\n		<v:f eqn=\"prod @3 21600 pixelWidth\"/> " +
+	 * "\n		<v:f eqn=\"prod @3 21600 pixelHeight\"/> " +
+	 * "\n		<v:f eqn=\"sum @0 0 1\"/> " + "\n		<v:f eqn=\"prod @6 1 2\"/> " +
+	 * "\n		<v:f eqn=\"prod @7 21600 pixelWidth\"/> " +
+	 * "\n		<v:f eqn=\"sum @8 21600 0\"/> " +
+	 * "\n		<v:f eqn=\"prod @7 21600 pixelHeight\"/> " +
+	 * "\n		<v:f eqn=\"sum @10 21600 0\"/> " + "\n	</v:formulas> " +
+	 * "\n		<v:path o:extrusionok=\"f\" gradientshapeok=\"t\" o:connecttype=\"rect\"/> "
+	 * + "\n		<o:lock v:ext=\"edit\" aspectratio=\"t\"/> " +
+	 * "\n	</v:shapetype> " +
+	 * "\n<w:binData w:name=\"wordml:\01000002.gif\" xml:space=\"preserve\">" +
+	 * "R0lGODlhHwAaAMQAAMjIyOHh4cLCwufn56ioqKOjo+zs7L29va6urs3NzdLS0ri4uNfX19zc3LOz"
+	 * +
+	 * "s/Hx8Z6engAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAAA"
+	 * +
+	 * "AAAALAAAAAAfABoAAAWv4COOZDCQ6Gg0aRs40NGSQAGd82MIUH/PDYJPNgsgfL1FC4DsBVy2JoRF"
+	 * +
+	 * "OkghjtQgKhWQeNcCbgSTIqiixhUCMKDU3VQZSXimrM2EVkpwt7g9WSkJUmgoAVIKLQtNBDkMUn4o"
+	 * +
+	 * "R0heM0xNDpqalnybmnYPmGtYIqQ+aKNrgqc9qa2sra+nsaezpLWkt6umsiOqV7lru8G9tr+wxrrI"
+	 * + "tMrDIwqf0pwi09OhOdna29wpIQA7" + "</w:binData>" +
+	 * "\n	<v:shape id=\"_x0000_i1026\" type=\"#_x0000_t75\" style=\"width:31pt;height:26pt\"><v:imagedata src=\"wordml:\01000002.gif\" o:title=\"quote.gif\"/>"
+	 * + "\n	</v:shape>" + "\n</w:pict>";
+	 */
 }
--- trunk/j2w-ejb/src/test/java/word/w2004/ImageTest.java
+++ trunk/j2w-ejb/src/test/java/word/w2004/ImageTest.java
@@ -1,30 +1,28 @@
 package word.w2004;
 
 import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
 
 import junit.framework.Assert;
 
+import org.jboss.seam.util.Base64;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import word.utils.TestUtils;
 import word.utils.Utils;
 import word.w2004.elements.Image;
-import org.jboss.seam.util.Base64;
+import word.w2004.elements.ImageType;
 
 public class ImageTest extends Assert {
 
 	@Test
-	public void sanityTest(){
-		Image img = new Image(Utils.getAppRoot() + "/src/test/resources/dtpick.gif");
+	public void sanityTest() throws IOException{
+		Image img = new Image(Utils.getAppRoot() + "/src/test/resources/dtpick.gif", ImageType.FULL_LOCAL_PATH);
 		//Image img = new Image(Utils.getAppRoot() + "/src/test/resources/base2logo.png");
 		// Image("/Users/leonardo_correa/Desktop/icons_corrup/quote.gif");
 
@@ -34,23 +32,43 @@ public class ImageTest extends Assert {
 		assertEquals(2, TestUtils.regexCount(img.getContent(), "<*v:shape[ >]")); //white space or >
 		assertEquals(2, TestUtils.regexCount(img.getContent(), "wordml"));
 		//for dtPicker.gif
-		assertEquals(1, TestUtils.regexCount(img.getContent(), "R0lGODlhEAAQAKIAAKVNSkpNpUpNSqWmpdbT1v///////wAAACH5BAEAAAYALAAAAAAQABAAAANE\naLrcNjDKKUa4OExYM95DVRTEWJLmKKLseVZELMdADcSrOwK7OqQsXkEIm8lsN0IOqCssW8Cicar8\nQa/P5kvA7Xq/ggQAOw=="));
+		assertEquals(1, TestUtils.regexCount(img.getContent(), "R0lGODlhEAAQAPMAAKVNSkpNpUpNSqWmpdbT1v///////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\nAAAAACH5BAEAAAYALAAAAAAQABAAQwRI0MhJqxmlkLwLyF8hYBpnluJArGzbjkEsB0NtD6PLAjyw\njqeOMANEDVGjm1IJm8WWONLxWDyGQjkdoecjVIOnrzEsKJvPaEEEADs="));
 
+	}
+	
+	@Test
+	public void localImageTest(){
+		Image img = new Image(Utils.getAppRoot() + "/src/test/resources/dtpick.gif", ImageType.FULL_LOCAL_PATH);
+		assertEquals(2, TestUtils.regexCount(img.getContent(), "<*w:pict>"));
+		assertEquals(2, TestUtils.regexCount(img.getContent(), "<*v:shapetype"));
+		assertEquals(2, TestUtils.regexCount(img.getContent(), "<*v:shape[ >]")); //white space or >
+		assertEquals(2, TestUtils.regexCount(img.getContent(), "wordml"));
+		assertEquals(1, TestUtils.regexCount(img.getContent(), "R0lGODlhEAAQAPMAAKVNSkpNpUpNS"));//just the beginning of...		
+	}
+	
+	@Test
+	public void webImageTest(){
+		Image img = new Image("http://www.google.com.au/intl/en_com/images/srpr/logo1w.png", ImageType.WEB_URL);
+		assertEquals(2, TestUtils.regexCount(img.getContent(), "<*w:pict>"));
+		assertEquals(2, TestUtils.regexCount(img.getContent(), "<*v:shapetype"));
+		assertEquals(2, TestUtils.regexCount(img.getContent(), "<*v:shape[ >]")); //white space or >
+		assertEquals(2, TestUtils.regexCount(img.getContent(), "wordml"));
+		assertEquals(1, TestUtils.regexCount(img.getContent(), "width:275pt;height:95pt"));
+		assertEquals(1, TestUtils.regexCount(img.getContent(), "BiGQFiipCSS8DCm1Cya1FiyNKzexKTjDDSrLDSvUDi3MEyzHFSvUFC3TGi7bGi/aEi7dGzLcFzPN"));		
 	}
 
 	@Test
-	public void defaultSizeTest() {
+	public void defaultSizeTest() throws IOException {
 		Image img = new Image(Utils.getAppRoot()
-				+ "/src/test/resources/base2logo.png");
-		// System.out.println(img.getContent());
+				+ "/src/test/resources/base2logo.png", ImageType.FULL_LOCAL_PATH);
 		assertEquals(1, TestUtils.regexCount(img.getContent(),
 				"style=\"width:116pt;height:104pt\""));
 	}
 
 	@Test
-	public void widthTest() {
+	public void widthTest() throws IOException {
 		Image img = new Image(Utils.getAppRoot()
-				+ "/src/test/resources/base2logo.png");
+				+ "/src/test/resources/base2logo.png", ImageType.FULL_LOCAL_PATH);
 		img.setWidth("120");
 		assertEquals(0, TestUtils.regexCount(img.getContent(),
 				"style=\"width:116pt;height:104pt\""));
@@ -59,9 +77,9 @@ public class ImageTest extends Assert {
 	}
 
 	@Test
-	public void heightTest() {
+	public void heightTest() throws IOException {
 		Image img = new Image(Utils.getAppRoot()
-				+ "/src/test/resources/base2logo.png");
+				+ "/src/test/resources/base2logo.png", ImageType.FULL_LOCAL_PATH);
 		img.setHeight("110");
 		assertEquals(0, TestUtils.regexCount(img.getContent(),
 				"style=\"width:116pt;height:104pt\""));
@@ -70,9 +88,9 @@ public class ImageTest extends Assert {
 	}
 
 	@Test
-	public void widthAndHeightTest() {
+	public void widthAndHeightTest() throws IOException {
 		Image img = new Image(Utils.getAppRoot()
-				+ "/src/test/resources/base2logo.png");
+				+ "/src/test/resources/base2logo.png", ImageType.FULL_LOCAL_PATH);
 		img.setWidth("121");
 		img.setHeight("111");
 		assertEquals(0, TestUtils.regexCount(img.getContent(),
@@ -80,14 +98,14 @@ public class ImageTest extends Assert {
 		assertEquals(1, TestUtils.regexCount(img.getContent(),
 				"style=\"width:121pt;height:111pt\""));
 	}
+	
+	
 
-	@Test
-	public void getOriginalWidthHeightTest() {
+	@Test(expected = java.lang.RuntimeException.class)
+	public void invalidImageTest(){
+		@SuppressWarnings("unused")
 		Image img = new Image(Utils.getAppRoot()
-				+ "/src/test/resources/whatever");
-		System.out.println("IIOException is expected ok...");
-		String res = img.getOriginalWidthHeight();
-		assertEquals("error", res);
+				+ "/src/test/resources/whatever", ImageType.FULL_LOCAL_PATH);
 	}
 
 	@Ignore
