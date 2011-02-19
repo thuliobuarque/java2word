6ea5359840726c8ac87fca419a42f9e2543786c7
leonardoanalista <leonardoanalista@7405c908-b26d-b50d-16d8-df60715af405>
2011-02-19 23:14:20 +0000

    comments updated

--- trunk/j2w-ejb/src/main/java/word/w2004/elements/PageBreak.java
+++ trunk/j2w-ejb/src/main/java/word/w2004/elements/PageBreak.java
@@ -2,6 +2,10 @@ package word.w2004.elements;
 
 import word.api.interfaces.IElement;
 
+/*
+ *  It inserts a Page break at the point you add this class to the document.
+ * 
+ */
 public class PageBreak implements IElement{
 
 	public String getContent() {
