82922a394aedfe90c8c703031a64edcc5bd0c8eb
leonardoanalista <leonardoanalista@7405c908-b26d-b50d-16d8-df60715af405>
2011-02-20 01:28:27 +0000

    Done Fluent interface for headings2 and 3, created All in one unit test example

--- j2w-ejb/src/main/java/word/w2004/elements/BreakLine.java
+++ j2w-ejb/src/main/java/word/w2004/elements/BreakLine.java
@@ -1,6 +1,7 @@
 package word.w2004.elements;
 
 import word.api.interfaces.IElement;
+import word.api.interfaces.IFluentElement;
 
 /**
  * 
@@ -15,7 +16,7 @@ import word.api.interfaces.IElement;
  *This will insert 3 Breaklines
  *
  */
-public class BreakLine implements IElement{
+public class BreakLine implements IElement, IFluentElement<BreakLine>{
 
 	private int times = 1;
 
@@ -42,5 +43,20 @@ public class BreakLine implements IElement{
 	 */
 	public BreakLine(){
 	}
+
+	/**
+	 * Created Breaklines according to the number of times provided.
+	 * @param value number of times
+	 * @return the Breakline object ready to go!
+	 */
+	public static BreakLine times(Integer value) {
+		return new BreakLine(value);
+	}
 	
-}
+	
+	@Override
+	public BreakLine create() {
+		return this;
+	}
+	
+}	
--- j2w-ejb/src/main/java/word/w2004/elements/Heading3.java
+++ j2w-ejb/src/main/java/word/w2004/elements/Heading3.java
@@ -1,9 +1,10 @@
 package word.w2004.elements;
 
+import word.api.interfaces.IFluentElement;
 import word.w2004.style.HeadingStyle;
 
 
-public class Heading3 extends AbstractHeading <HeadingStyle> {
+public class Heading3 extends AbstractHeading <HeadingStyle> implements IFluentElement<Heading3>{
 
 	//Constructor
 	public Heading3(String value){
@@ -13,6 +14,11 @@ public class Heading3 extends AbstractHeading <HeadingStyle> {
 	
 	public static Heading3 with(String string) {
 		return new Heading3(string);
+	}
+
+	@Override
+	public Heading3 create() {
+		return this;
 	}
 	
 }
