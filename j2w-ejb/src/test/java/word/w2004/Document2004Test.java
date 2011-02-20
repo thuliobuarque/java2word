a7d718c1ade248e04cf7b6fd2f18cf989efef2b6
leonardoanalista <leonardoanalista@7405c908-b26d-b50d-16d8-df60715af405>
2011-02-20 02:29:32 +0000

    Fluent for images, unit test for All-in-one

--- j2w-ejb/src/test/java/word/w2004/Document2004Test.java
+++ j2w-ejb/src/test/java/word/w2004/Document2004Test.java
@@ -217,137 +217,144 @@ public class Document2004Test extends Assert {
 	}
 
 	
-	@Ignore //ignored by default just to not create files in your system or break the build...
-	@Test
-	public void testJava2wordAllInOne() {
+	//@Ignore //ignored by default just to not create files in your system or break the build...
+@Test
+public void testJava2wordAllInOne() {
 
-		File fileObj = new File("/home/leonardo/Desktop/Java2word_allInOne.doc");
+	File fileObj = new File("/home/leonardo/Desktop/Java2word_allInOne.doc");
 
-		PrintWriter writer = null;
-		try {
-			writer = new PrintWriter(fileObj);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		IDocument myDoc = new Document2004();
-		
-		myDoc.addEle(BreakLine.times(1).create()); // this is one breakline
-
-		//Headings
-		myDoc.addEle(Heading2.with("===== Headings ======").create());
-		myDoc.addEle(Paragraph
-				.with("I will try to use a little bit of everything in the API Java2word. " +
-						"I realised that it is very dificult to keep the doucmentation updated " +
-						"so this is where I will demostrate how to do some cool things with Java2word!")
-				.create());
-
-		myDoc.addEle(Heading1.with("Heading01 without styling").create());
-		myDoc.addEle(Heading2.with("Heading02 with styling").withStyle()
-				.setAlign(Align.CENTER).setItalic(true).create());
-		myDoc.addEle(Heading3.with("Heading03").withStyle().setBold(true)
-				.setAlign(Align.RIGHT).create());
-
-		//Paragraph and ParagrapPiece
-		myDoc.addEle(Heading2.with("===== Paragraph and ParagrapPiece ======").create());
-		myDoc.addEle(Paragraph.with("I am a very simple paragraph.").create());
-		
-		myDoc.addEle(BreakLine.times(1).create()); 		
-		ParagraphPiece myParPiece01 = new ParagraphPiece("If you use the class 'Paragraph', you will have limited style. Maybe only paragraph aligment.");
-		ParagraphPiece myParPiece02 = new ParagraphPiece("In order to use more advanced style, you have to use ParagraphPiece");
-		ParagraphPiece myParPiece03 = new ParagraphPiece("One example of this is when you want to make ONLY one word BOLD or ITALIC. the way to to this is create many pieces, format them separetely and put all together in a Paragraph object. Example:");
-		
-		myDoc.addEle(Paragraph.withPieces(myParPiece01, myParPiece02, myParPiece03).create());
-		
-		ParagraphPiece myParPieceJava = new ParagraphPiece("I like Java and ").withStyle().setFont(Font.COURIER).create();
-		ParagraphPiece myParPieceRuby = new ParagraphPiece("Ruby!!! ").withStyle().setBold(true).setItalic(true).create();
-		ParagraphPiece myParPieceAgile = new ParagraphPiece("I actully love Java, Ruby Agile, BDD, Cucumber, automation... ").withStyle().setTextColor("008000").create();
-				
-		myDoc.addEle(Paragraph.withPieces(myParPieceJava, myParPieceRuby, myParPieceAgile).create());
-		
-		
-		//Document Header and Footer
-		myDoc.addEle(BreakLine.times(2).create()); 
-		myDoc.addEle(Heading2.with("===== Document Header and Footer ======").create());
-		myDoc.addEle(Paragraph.with("By default everything is added to the Body when you do 'myDoc.addEle(...)'." +
-				" But you can add elements to the Header and/or Footer. Other cool thing is show page number or not.").create());
-		
-		myDoc.addEle(BreakLine.times(2).create()); 
-		myDoc.addEle(Paragraph.with("Page number is displayed by default but you can disable: 'myDoc.getFooter().showPageNumber(false)' ").create());
-		
-		myDoc.addEle(BreakLine.times(2).create()); 
-		myDoc.addEle(Paragraph.with("you can also hide Header and Footer in the first Page. This is useful for when you have a cover page.: 'myDoc.getHeader().setHideHeaderAndFooterFirstPage(true)' ").create());
-		
-		
-		myDoc.getHeader().addEle(Paragraph.withPieces(ParagraphPiece.with("I am in the"), ParagraphPiece.with(" Header ").withStyle().setBold(true).create(), ParagraphPiece.with("of all pages")).create());
-		
-		myDoc.getFooter().addEle(Paragraph.with("I am in the Footer of all pages").create());
-				
-		
-		//Images
-		myDoc.addEle(BreakLine.times(1).create()); 
-		myDoc.addEle(Heading2.with("===== Images ======").create());
-		myDoc.addEle(Paragraph.with("Images can be created from diferent locations. It can be from your local machine, from web URL or classpath.").create());
-		
-		myDoc.addEle(Paragraph.with("This one is coming from WEB, google web site: ").create());		
-		myDoc.addEle(new Image("http://www.google.com/images/logos/ps_logo2.png", ImageLocation.WEB_URL));
-
-		
-		myDoc.addEle(BreakLine.times(2).create());
-		myDoc.addEle(Paragraph.with("You can always be creative mixing up images inside other IElements. Eg.: Paragraphs, Tables, etc.").create());
-		
-		myDoc.addEle(
-				new Paragraph("This document inside the paragraph, coming from '/src/test/resources/dtpick.gif': "
-						+ new Image(Utils.getAppRoot()
-								+ "/src/test/resources/dtpick.gif",
-								ImageLocation.FULL_LOCAL_PATH).getContent()));
-				
-		myDoc.addEle(BreakLine.times(1).create()); 
-
-		
-		//Table
-		myDoc.addEle(Heading2.with("===== Table ======").create());
-		myDoc.addEle(Paragraph.with("Table os soccer playerd and their number of gols - the best of the best of all times:").create());
-		myDoc.addEle(BreakLine.times(1).create()); 
-		
-		Table tbl = new Table();
-		tbl.addTableEle(TableEle.TH, "Name", "Number of gols", "Country");
-
-		tbl.addTableEle(TableEle.TD, "Arthur Friedenreich", "1329", "Brazil");
-		tbl.addTableEle(TableEle.TD, "Pele", "1281", "Brazil");
-		tbl.addTableEle(TableEle.TD, "Romario", "1002",  "Brazil");
-		tbl.addTableEle(TableEle.TD, "Tulio Maravilha", "956",  "Brazil");
-		tbl.addTableEle(TableEle.TD, "Zico", "815",  "Brazil");
-		tbl.addTableEle(TableEle.TD, "Roberto Dinamite", "748",  "Brazil");
-		tbl.addTableEle(TableEle.TD, "Di Stéfano", "715", "Argentina");
-		tbl.addTableEle(TableEle.TD, "Puskas", "689", "Hungary");
-		tbl.addTableEle(TableEle.TD, "Flávio", "591", "Brazil");
-		tbl.addTableEle(TableEle.TD, "James McGory", "550", "Scotland");
-		tbl.addTableEle(TableEle.TD, "Leonardo Correa", "299", "Brazil/Australia");
-		
-		tbl.addTableEle(TableEle.TF, "Total", "1,100,000.00", " ");
-		
-		myDoc.addEle(tbl);
-		
-		myDoc.addEle(BreakLine.times(1).create()); 
-		
-		myDoc.addEle(Paragraph.withPieces(ParagraphPiece.with("* Zico was mid-fieldfer and managed to score all those fucking goals!").withStyle().setItalic(true).create()).create());
-		myDoc.addEle(Paragraph.withPieces(ParagraphPiece.with("* Leonardo Correa's goals (me) include futsal, soccer, friendly games, training games, so on... (but not playstation)").withStyle().setItalic(true).create()).create());
-
-		
-		//PageBreaks
-		myDoc.addEle(Heading2.with("===== PageBreak ======").create());
-		myDoc.addEle(Paragraph.with("There is a PAGE BREAK after this line:").create());
-		myDoc.addEle(PageBreak.create());
-		myDoc.addEle(Paragraph.with("There is a PAGE BREAK before this line:").create());
-
-		
-		
-		String myWord = myDoc.getContent();
-
-		writer.println(myWord);
-		writer.close();
+	PrintWriter writer = null;
+	try {
+		writer = new PrintWriter(fileObj);
+	} catch (FileNotFoundException e) {
+		e.printStackTrace();
 	}
+
+	IDocument myDoc = new Document2004();
+	
+	myDoc.addEle(BreakLine.times(1).create()); // this is one breakline
+
+	//Headings
+	myDoc.addEle(Heading2.with("===== Headings ======").create());
+	myDoc.addEle(Paragraph.with("This doc has been generated by the unit test testJava2wordAllInOne() in the class DocumentTest2004Test.java.").create());
+	myDoc.addEle(BreakLine.times(1).create());
+	
+	myDoc.addEle(Paragraph
+			.with("I will try to use a little bit of everything in the API Java2word. " +
+					"I realised that is very dificult to keep the doucmentation updated " +
+					"so this is where I will demostrate how to do some cool things with Java2word!")
+			.create());
+	
+
+	myDoc.addEle(Heading1.with("Heading01 without styling").create());
+	myDoc.addEle(Heading2.with("Heading02 with styling").withStyle()
+			.setAlign(Align.CENTER).setItalic(true).create());
+	myDoc.addEle(Heading3.with("Heading03").withStyle().setBold(true)
+			.setAlign(Align.RIGHT).create());
+
+	//Paragraph and ParagrapPiece
+	myDoc.addEle(Heading2.with("===== Paragraph and ParagrapPiece ======").create());
+	myDoc.addEle(Paragraph.with("I am a very simple paragraph.").create());
+	
+	myDoc.addEle(BreakLine.times(1).create()); 		
+	ParagraphPiece myParPiece01 = new ParagraphPiece("If you use the class 'Paragraph', you will have limited style. Maybe only paragraph aligment.");
+	ParagraphPiece myParPiece02 = new ParagraphPiece("In order to use more advanced style, you have to use ParagraphPiece");
+	ParagraphPiece myParPiece03 = new ParagraphPiece("One example of this is when you want to make ONLY one word BOLD or ITALIC. the way to to this is create many pieces, format them separetely and put all together in a Paragraph object. Example:");
+	
+	myDoc.addEle(Paragraph.withPieces(myParPiece01, myParPiece02, myParPiece03).create());
+	
+	ParagraphPiece myParPieceJava = new ParagraphPiece("I like Java and ").withStyle().setFont(Font.COURIER).create();
+	ParagraphPiece myParPieceRuby = new ParagraphPiece("Ruby!!! ").withStyle().setBold(true).setItalic(true).create();
+	ParagraphPiece myParPieceAgile = new ParagraphPiece("I actully love Java, Ruby Agile, BDD, Cucumber, automation... ").withStyle().setTextColor("008000").create();
+			
+	myDoc.addEle(Paragraph.withPieces(myParPieceJava, myParPieceRuby, myParPieceAgile).create());
+	
+	
+	//Document Header and Footer
+	myDoc.addEle(BreakLine.times(2).create()); 
+	myDoc.addEle(Heading2.with("===== Document Header and Footer ======").create());
+	myDoc.addEle(Paragraph.with("By default everything is added to the Body when you do 'myDoc.addEle(...)'." +
+			" But you can add elements to the Header and/or Footer. Other cool thing is show page number or not.").create());
+	
+	myDoc.addEle(BreakLine.times(2).create()); 
+	myDoc.addEle(Paragraph.with("Page number is displayed by default but you can disable: 'myDoc.getFooter().showPageNumber(false)' ").create());
+	
+	myDoc.addEle(BreakLine.times(2).create()); 
+	myDoc.addEle(Paragraph.with("you can also hide Header and Footer in the first Page. This is useful for when you have a cover page.: 'myDoc.getHeader().setHideHeaderAndFooterFirstPage(true)' ").create());
+	
+	myDoc.getHeader().addEle(Paragraph.withPieces(ParagraphPiece.with("I am in the"), ParagraphPiece.with(" Header ").withStyle().setBold(true).create(), ParagraphPiece.with("of all pages")).create());
+	
+	myDoc.getFooter().addEle(Paragraph.with("I am in the Footer of all pages").create());
+			
+	
+	//Images
+	myDoc.addEle(BreakLine.times(1).create()); 
+	myDoc.addEle(Heading2.with("===== Images ======").create());
+	myDoc.addEle(Paragraph.with("Images can be created from diferent locations. It can be from your local machine, from web URL or classpath.").create());
+	
+	myDoc.addEle(Paragraph.with("This one is coming from WEB, google web site: ").create());		
+	myDoc.addEle(new Image("http://www.google.com/images/logos/ps_logo2.png", ImageLocation.WEB_URL));
+	
+	myDoc.addEle(BreakLine.times(2).create()); 
+	myDoc.addEle(Paragraph.with("You can change the image dimensions:.").create());		
+	myDoc.addEle(Image.from_WEB_URL("http://www.google.com/images/logos/ps_logo2.png").setHeight("40").setWidth("80").create());
+
+	
+	myDoc.addEle(BreakLine.times(2).create());
+	myDoc.addEle(Paragraph.with("You can always be creative mixing up images inside other IElements. Eg.: Paragraphs, Tables, etc.").create());
+	
+	myDoc.addEle(
+			new Paragraph("This document inside the paragraph, coming from '/src/test/resources/dtpick.gif': "
+					+ new Image(Utils.getAppRoot()
+							+ "/src/test/resources/dtpick.gif",
+							ImageLocation.FULL_LOCAL_PATH).getContent()));
+			
+	myDoc.addEle(BreakLine.times(1).create()); 
+
+	
+	//Table
+	myDoc.addEle(Heading2.with("===== Table ======").create());
+	myDoc.addEle(Paragraph.with("Table os soccer playerd and their number of gols - the best of the best of all times:").create());
+	myDoc.addEle(BreakLine.times(1).create()); 
+	
+	Table tbl = new Table();
+	tbl.addTableEle(TableEle.TH, "Name", "Number of gols", "Country");
+
+	tbl.addTableEle(TableEle.TD, "Arthur Friedenreich", "1329", "Brazil");
+	tbl.addTableEle(TableEle.TD, "Pele", "1281", "Brazil");
+	tbl.addTableEle(TableEle.TD, "Romario", "1002",  "Brazil");
+	tbl.addTableEle(TableEle.TD, "Tulio Maravilha", "956",  "Brazil");
+	tbl.addTableEle(TableEle.TD, "Zico", "815",  "Brazil");
+	tbl.addTableEle(TableEle.TD, "Roberto Dinamite", "748",  "Brazil");
+	tbl.addTableEle(TableEle.TD, "Di Stéfano", "715", "Argentina");
+	tbl.addTableEle(TableEle.TD, "Puskas", "689", "Hungary");
+	tbl.addTableEle(TableEle.TD, "Flávio", "591", "Brazil");
+	tbl.addTableEle(TableEle.TD, "James McGory", "550", "Scotland");
+	tbl.addTableEle(TableEle.TD, "Leonardo Correa", "299", "Brazil/Australia");
+	
+	tbl.addTableEle(TableEle.TF, "Total", "1,100,000.00", " ");
+	
+	myDoc.addEle(tbl);
+	
+	myDoc.addEle(BreakLine.times(1).create()); 
+	
+	myDoc.addEle(Paragraph.withPieces(ParagraphPiece.with("* Zico was mid-fieldfer and managed to score all those fucking goals!").withStyle().setItalic(true).create()).create());
+	myDoc.addEle(Paragraph.withPieces(ParagraphPiece.with("* Leonardo Correa's goals (me) include futsal, soccer, friendly games, training games, so on... (but not playstation)").withStyle().setItalic(true).create()).create());
+
+	
+	//PageBreaks
+	myDoc.addEle(Heading2.with("===== PageBreak ======").create());
+	myDoc.addEle(Paragraph.with("There is a PAGE BREAK after this line:").create());
+	myDoc.addEle(PageBreak.create());
+	myDoc.addEle(Paragraph.with("There is a PAGE BREAK before this line:").create());
+
+	
+	
+	String myWord = myDoc.getContent();
+
+	writer.println(myWord);
+	writer.close();
+}
 
 	@Ignore
 	@Test
