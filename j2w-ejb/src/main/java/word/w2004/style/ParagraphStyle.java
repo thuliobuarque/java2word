6f5a32b34d9ddb52c79c806f24913997bd07ec05
leonardoanalista <leonardoanalista@7405c908-b26d-b50d-16d8-df60715af405>
2011-03-21 03:52:23 +0000

    Added BgColor for paragraph and paragraphPieces

--- j2w-ejb/src/main/java/word/w2004/style/ParagraphStyle.java
+++ j2w-ejb/src/main/java/word/w2004/style/ParagraphStyle.java
@@ -5,45 +5,85 @@ import word.api.interfaces.ISuperStylin;
 
 /**
  * @author anyone
- * 
+ *
  * Use this class to apply style to the Paragraph - the whole paragraph. At the moment is only possible to apply Align.
  * If you want to apply bold, italic or underline, use a ParagraphPiece to do that.
- * 
+ *
  */
 public class ParagraphStyle extends AbstractStyle implements ISuperStylin{
 
-	private Align align = Align.LEFT;
+    private Align align = Align.LEFT;
+    private String bgColor = "";
 
-	public enum Align { 
-		CENTER("center"), LEFT("left"), RIGHT("right"), JUSTIFIED("both");
-		private String value;
+    public enum Align {
+        CENTER("center"), LEFT("left"), RIGHT("right"), JUSTIFIED("both");
+        private String value;
 
-		Align(String value) {
-			this.value = value;
-		}
+        Align(String value) {
+            this.value = value;
+        }
 
-		public String getValue() {
-			return value;
-		}
-	}
-	
-	@Override
-	public String getNewContentWithStyle(String txt) {
-		String styleValue = "\n		<w:pPr>\n		" +
-							"	<w:jc w:val=\"" + align.getValue()+ "\"/> \n	" +
-							"		{styleText}\n 	" +
-							"	</w:pPr>";
-		txt = txt.replace("{styleAlign}", styleValue);
-		txt = txt.replaceAll("[{]style(.*)[}]", ""); //Convention: replace unused styles after... 
-		return txt;
-	}
-	
-	
-	//### Getters and setters... ###
+        public String getValue() {
+            return value;
+        }
+    }
 
-	public ParagraphStyle setAlign(Align align) {
-		this.align = align;
-		return this;
-	}
+    @Override
+    public String getNewContentWithStyle(String txt) {
+        StringBuilder style = new StringBuilder("");
+
+        //There will be always align 'Left' by default
+        doStyleAlignment(style);
+        doStyleBgColor(style);
+
+        return doStyleReplacement(style, txt);
+    }
+
+
+    private void doStyleAlignment(StringBuilder style) {
+        style.append("  <w:jc w:val=\"" + align.getValue()+ "\"/> \n    " + "       {styleText}\n   ");
+    }
+
+    private void doStyleBgColor(StringBuilder style) {
+        if (!bgColor.equals("")) {
+            style.append("\n            <w:shd w:val=\"clear\" w:color=\"auto\" w:fill=\""+ bgColor +"\" />\n");
+        }
+    }
+
+    private String doStyleReplacement(StringBuilder style, String txt) {
+
+       // if (!"".equals(style.toString())) {
+            style.insert(0, "\n  <w:pPr>");
+            style.append("\n     </w:pPr>");
+            txt = txt.replace("{styleText}", style.toString());// Convention:
+                                                               // apply styles
+       // }
+
+        //txt = txt.replace("{styleText}", styleValue);
+        txt = txt.replaceAll("[{]style(.*)[}]", ""); //Convention: replace unused styles after...
+        return txt;
+    }
+
+
+    //### Getters and setters... ###
+
+    /**
+     * If you know the color code, just to straight to the point! Eg.: yellow:
+     * FFFF00, black: 000000, red: FF0000, blue: 0000FF, green: 008000, etc...
+     *
+     * If you want, you can use the class Color.whatever_color.
+     *
+     * @param hexadecimal
+     *            color code
+     */
+    public ParagraphStyle setBgColor(String bgColor) {
+        this.bgColor = bgColor;
+        return this;
+    }
+
+    public ParagraphStyle setAlign(Align align) {
+        this.align = align;
+        return this;
+    }
 
 }
