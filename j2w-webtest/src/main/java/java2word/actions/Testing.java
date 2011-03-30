08c3f88fcf16c5e0f3cf53fdbe57ed29d8f49f38
leonardoanalista <leonardoanalista@7405c908-b26d-b50d-16d8-df60715af405>
2011-03-30 05:51:33 +0000

    testing gradle scripts for build

--- j2w-webtest/src/main/java/java2word/actions/Testing.java
+++ j2w-webtest/src/main/java/java2word/actions/Testing.java
@@ -1,41 +1,51 @@
 package java2word.actions;
 
 import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.xwork.StringUtils;
+import org.apache.struts2.interceptor.ServletRequestAware;
 import org.apache.struts2.interceptor.ServletResponseAware;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class Testing extends ActionSupport implements ServletResponseAware {
+public class Testing extends ActionSupport implements ServletResponseAware, ServletRequestAware {
 
+    @Override
     public String execute() throws Exception {
 //		System.out.println("### About to generate Word doc...");
 
 //		System.out.println("XML is: \n" + this.xml + "\n");
+//###LEO
 
+        request.setAttribute("res", "@@@ " + new Date());
+        request.getSession().setAttribute("xml", xml);
 
         //'UTF-8', 'ISO-8859-1' or nothing? up to you...
         if (!StringUtils.isEmpty(xml)) {
-            servletResponse.setContentType("application/msword; charset=UTF-8");
-            servletResponse.setHeader("Content-disposition",
+            response.setContentType("application/msword; charset=UTF-8");
+            response.setHeader("Content-disposition",
                     "inline;filename=wordDoc.doc");
 
-            PrintWriter writer = servletResponse.getWriter();
+            PrintWriter writer = response.getWriter();
             writer.println(xml);
             writer.flush();
 //		System.out.println("### Doc generated...");
             return null;
         }else{
-            System.out.println("Error: Empty XML field!");
+            System.out.println("Error: Empty XML field porra!!!" );
         }
+
 
         return SUCCESS;
     }
 
     // ### Getters and setters
     private String xml;
-    private HttpServletResponse servletResponse;
+    private HttpServletResponse response;
+    private HttpServletRequest request;
 
     public String getXml() {
         return xml;
@@ -45,9 +55,15 @@ public class Testing extends ActionSupport implements ServletResponseAware {
         this.xml = xml;
     }
 
-    @Override
+    //@Override
     public void setServletResponse(HttpServletResponse servletResponse) {
-        this.servletResponse = servletResponse;
+        response = servletResponse;
     }
+
+    public void setServletRequest(HttpServletRequest servletRequest) {
+        request = servletRequest;
+    }
+
+
 
 }
