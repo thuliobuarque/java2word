9759b865012dfc787dd67f3720ef5c4fd87e0c8c
leonardoanalista <leonardoanalista@7405c908-b26d-b50d-16d8-df60715af405>
2012-02-11 02:14:00 +0000

    WIP: Table V2 is almost ready, unit test yet to come

--- trunk/java2word/src/main/java/word/w2004/elements/tableElements/TableCell.java
+++ trunk/java2word/src/main/java/word/w2004/elements/tableElements/TableCell.java
@@ -9,19 +9,12 @@ public class TableCell implements IElement, IFluentElement<TableCell>, IFluentEl
 	
 	StringBuilder txt = new StringBuilder("");
 	private TableCellStyle style = new TableCellStyle();
-	private boolean hasBeenCalledBefore = false; 
+    private final String tableCellTop = "\n     <w:tc>\n        {styleCellPh}";
+    private final String tableCellBottom = "\n      </w:tc>";
 	
     @Override
     public String getContent() {
-    	System.out.println("### TableCell.getContent");
-//        if (hasBeenCalledBefore) {
-//            return txt.toString();
-//        } else {
-//            hasBeenCalledBefore = true;
-//        }
-    	
     	String withStyle = style.getNewContentWithStyle(txt.toString());
-    	System.out.println("@@@ withStyle: " + withStyle);
         return withStyle;
     }
     
@@ -30,21 +23,20 @@ public class TableCell implements IElement, IFluentElement<TableCell>, IFluentEl
      * */
     private TableCell() {
     }
-    
+
     private TableCell(Object cell) {		
     	
         if (cell instanceof String) { //new Par
-        	txt.append("\n		<w:tc>\n		{styleCellPh}");
+        	txt.append(tableCellTop);
         	txt.append(Paragraph.with(cell.toString()).create().getContent());
-        	txt.append("\n		</w:tc>");    	
+        	txt.append(tableCellBottom);    	
         } else if (cell instanceof IElement) {
 
         	if(cell instanceof TableCell){ 
         		//simple do a getContent because object is already a TableCell
-        		txt.append("\n		<w:tc>\n		{styleCellPh}");
+        		txt.append(tableCellTop);
         		txt.append(((TableCell) cell).txt.toString()); //it is a Paragraph at this moment
-        		
-        		txt.append("\n		</w:tc>");  
+        		txt.append(tableCellBottom);  
         	}else{
         		//Paragraph for example...
         		//apply parent styles
@@ -57,7 +49,6 @@ public class TableCell implements IElement, IFluentElement<TableCell>, IFluentEl
                     "Parameter can only be String of IElement. You gave me: " + cell.getClass().toString());
         }
         
-        
     }
 
 	@Override
@@ -65,8 +56,24 @@ public class TableCell implements IElement, IFluentElement<TableCell>, IFluentEl
 		return this;
 	}
 	
+	
+    /**
+     * It can receive a String, TableCell or any other IElement.
+     * for String, text will be wrapped on a Paragraph.
+     * 
+     * Use TableCell when you need to apply style to the Cell. Eg.: Background color. 
+     *  PS.: If you need to apply "bold", "italic" or "underline" to the cell, use a Paragraph with its Pieces.
+     *  
+     * If you pass an IElement, the getContent() method will be blindly call. So be careful on what you pass. 
+     * Remember you can always wrap elements or build your own String XML that represents what you want - Be creative!   
+     * 
+     * You can also pass 2 different kinds objects at the same time. Eg.:
+     * <code>
+     * tbl.addRow( TableRowV2.with( TableCell.with(Paragraph.with("Paragraph01").create()), "Simple String" ).withStyle().bold().create() );
+     * </code>
+     *   
+     **/
 	public static TableCell with(Object cell) {
-		
 		if(cell instanceof TableCell){
 			//it is already TableCell so no need to create another instance
 			//it is a Paragraph at this moment. It needs Column Top and Bottom    		
